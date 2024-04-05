a73194a46b445d776c397dc66d9e096bb1c76e1f
wls <ufo281@outlook.com>
2024-04-05 17:44:04 +0800

    newst

---
+++ 8051MCU_example/C_Language/SDCC_Version/1.0_GPIO/auto_compile.sh
@@ -0,0 +1,1 @@
+sdcc main.c
---
+++ 8051MCU_example/C_Language/SDCC_Version/1.0_GPIO/main.c
@@ -0,0 +1,24 @@
+/**
+ * @file main.c
+ * @author wls
+ * 
+ * @brief 
+ * @version 0.1
+ * @date 2024-04-05
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+
+
+
+
+
+void main(void)
+{
+    
+
+}
+ 
+
